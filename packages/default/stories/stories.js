a22b27da64f35c8b1427f1120fefce8fe274324b
Grant Chatterton <grantchatterton@gmail.com>
2025-07-07 13:25:05 -0500

    Add "Cache-Control" header in response

--- packages/default/stories/stories.js
+++ packages/default/stories/stories.js
@@ -2,5 +2,10 @@ import { getTopStories } from "./util.js";
 
 export async function main() {
   const stories = await getTopStories();
-  return { body: stories };
+  return {
+    headers: {
+      "Cache-Control": "public, max-age=3600",
+    },
+    body: stories,
+  };
 }
