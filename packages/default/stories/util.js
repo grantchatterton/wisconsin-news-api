4fb26f321891d5da87b85bf5d00bda70c1d6dfa5
Grant Chatterton <grantchatterton@gmail.com>
2025-07-07 13:25:24 -0500

    Fix bugs and improve code

--- packages/default/stories/util.js
+++ packages/default/stories/util.js
@@ -4,13 +4,33 @@ import * as cheerio from "cheerio";
 // URL for retrieving the top news stories
 const NEWS_URL = "https://www.channel3000.com/news/local-news";
 
+// Create an instance to use for Axios
+// We do this to circumvent the 429 - Too Many Requests response
+const instance = axios.create({
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
+  },
+});
+
+/**
+ * Fetches the HTML of a given web-page and returns a CheerioAPI object representing it.
+ *
+ * @param url     URL to fetch HTML of.
+ * @returns       CheerioAPI object for parsing the HTML.
+ */
+export async function getHTMLFromUrl(url) {
+  const response = await instance.get(url);
+  return cheerio.load(response.data);
+}
+
 /**
  * Fetches an array of the top story URLs.
+ *
  * @returns Promise containing an array of story URLs.
  */
 export async function getTopStoryUrls() {
-  const response = await axios.get(NEWS_URL);
-  const $ = cheerio.load(response.data); // using cheerio.fromURL causes a redirect
+  const $ = await getHTMLFromUrl(NEWS_URL);
   return $(".list-popular .tnt-asset-link")
     .map(function () {
       return `${NEWS_URL}${$(this).attr("href")}`;
@@ -20,6 +40,7 @@ export async function getTopStoryUrls() {
 
 /**
  * Fetches an array of the top stories.
+ *
  * @returns Promise containing an array of story objects.
  */
 export async function getTopStories() {
@@ -28,12 +49,13 @@ export async function getTopStories() {
   return await Promise.all(
     urls.map(async (url) => {
       // fetch HTML for this url
-      const $ = await cheerio.fromURL(url);
+      const $ = await getHTMLFromUrl(url);
 
       // fetch various properties of the article
-      const title = $(".headline").text();
+      const title = $(".headline").text().trim();
       const description =
-        $(".subhead").text() || $("#article-body > p").first().text();
+        $(".subhead").text().trim() ||
+        $("#article-body > p").first().text().trim();
       const image =
         $(".asset-photo img").attr("src") ||
         "https://m.media-amazon.com/images/I/91ziU2NUHzL.png";
@@ -48,6 +70,7 @@ export async function getTopStories() {
 }
 
 export default {
+  getHTMLFromUrl,
   getTopStoryUrls,
   getTopStories,
 };
